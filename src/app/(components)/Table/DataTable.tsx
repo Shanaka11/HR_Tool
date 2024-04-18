2c36410f5e102d3ddf2a60fb9504c4306ee936c5
Shanaka Abeysinghe <Shanakaabeysinghe@gmail.com>
2024-04-18 10:29:03 +0530

    Added checkbox

--- src/app/(components)/Table/DataTable.tsx
+++ src/app/(components)/Table/DataTable.tsx
@@ -9,6 +9,7 @@ import {
 import React from "react";
 import { ColumnDef } from "./types";
 import { useTable } from "./useTable";
+import { Checkbox } from "@/components/ui/checkbox";
 
 type BaseDataItem = {
   id: string;
@@ -32,6 +33,17 @@ const DataTable = <T extends BaseDataItem>({
     <Table>
       <TableHeader>
         <TableRow>
+          <TableHead>
+            <Checkbox
+              className="block"
+              //   onClick={(event) => selectRow(rowIndex, event.shiftKey)}
+              //   checked={
+              //     rows.filter((row, index) => index === rowIndex && row.isSelected)
+              //       .length > 0
+              //   }
+              //   disabled={tableState === "NEW"}
+            />
+          </TableHead>
           {table.getHeaders().map((header) => (
             <TableHead key={header.id}>{header.header}</TableHead>
           ))}
@@ -40,6 +52,17 @@ const DataTable = <T extends BaseDataItem>({
       <TableBody>
         {table.getRows().map((row) => (
           <TableRow key={row.id}>
+            <TableCell>
+              <Checkbox
+                className="block"
+                //   onClick={(event) => selectRow(rowIndex, event.shiftKey)}
+                //   checked={
+                //     rows.filter((row, index) => index === rowIndex && row.isSelected)
+                //       .length > 0
+                //   }
+                //   disabled={tableState === "NEW"}
+              />
+            </TableCell>
             {table.getHeaders().map((column) => (
               <TableCell key={`${row.id}-${column.header}`}>
                 {table.getCellReadValue(row, column.id)}
